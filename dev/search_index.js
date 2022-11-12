02c48773b20001f64f81a05e9c7a0b7f2fe3be09
Documenter.jl <documenter@juliadocs.github.io>
2022-11-12 18:07:12 +0000

    build based on 0da4e05

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MathepiaOptimal","category":"page"},{"location":"#MathepiaOptimal","page":"Home","title":"MathepiaOptimal","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MathepiaOptimal.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MathepiaOptimal]","category":"page"}]
+}
